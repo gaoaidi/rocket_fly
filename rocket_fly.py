40607aa1e43996ab18e73a341a3720d7a9c367cd
gaoaidi <gaoaidibenny@163.com>
2021-08-13 19:29:26 +0800

    Add a rocket, bur icon is too big.

--- rocket_fly.py
+++ rocket_fly.py
@@ -1,4 +1,6 @@
 from settings import Settings
+from rocket import Rocket
+
 import sys
 import pygame
 
@@ -15,12 +17,14 @@ class RocketFly:
         self.bg_color=self.settings.bg_color
         # 初始化title
         pygame.display.set_caption("Rocket Fly")
+        # 初始化rocket
+        self.rocket=Rocket(self)
     def run_game(self):
         # 主循环程序
         while True:
             self._check_events()
             self._update_screen()
-
+            self.rocket.update()
     def _check_events(self):
         for event in pygame.event.get():
             if event.type==pygame.QUIT:
@@ -28,9 +32,27 @@ class RocketFly:
             if event.type==pygame.KEYDOWN:
                 if event.key==pygame.K_q:
                     sys.exit()
-    
+                if event.key==pygame.K_RIGHT:
+                    self.rocket.moving_right=True
+                if event.key==pygame.K_LEFT:
+                    self.rocket.moving_left=True
+                if event.key==pygame.K_UP:
+                    self.rocket.moving_up=True
+                if event.key==pygame.K_DOWN:
+                    self.rocket.moving_down=True
+            if event.type==pygame.KEYUP:
+                if event.key==pygame.K_RIGHT:
+                    self.rocket.moving_right=False
+                if event.key==pygame.K_LEFT:
+                    self.rocket.moving_left=False
+                if event.key==pygame.K_UP:
+                    self.rocket.moving_up=False
+                if event.key==pygame.K_DOWN:
+                    self.rocket.moving_down=False
+
     def _update_screen(self):
         self.screen.fill(self.bg_color)
+        self.rocket.blitme()
         pygame.display.update()
 
 if __name__=="__main__":
--- settings.py
+++ settings.py
@@ -1,6 +1,11 @@
 class Settings:
     def __init__(self):
+        # 屏幕设置
         self.screen_width=1200
         self.screen_height=800
 
+        # 背景颜色
         self.bg_color=(180,54,58)
+
+        # 速度设置
+        self.speed=1.5
